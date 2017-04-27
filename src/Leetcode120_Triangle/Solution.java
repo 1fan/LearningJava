68b81d96666577c64cac46663d183736516746f3
1fan <2577196927@qq.com>
2017-04-27 23:19:30 +1000

    Finish Reg_match DP solution

---
+++ src/Leetcode120_Triangle/Solution.java
@@ -0,0 +1,8 @@
+package Leetcode120_Triangle;
+
+/**
+ * Created by hasee on 2017/4/27.
+ */
+public class Solution {
+    
+}
--- src/Leetcode_RegularExpressionMatch/Solution3.java
+++ src/Leetcode_RegularExpressionMatch/Solution3.java
@@ -2,6 +2,47 @@ package Leetcode_RegularExpressionMatch;
 
 /**
  * Created by hasee on 2017/4/25.
+ *  isMatch("aa","a") → false
+ isMatch("aa","aa") → true
+ isMatch("aaa","aa") → false
+ isMatch("aa", "a*") → true
+ isMatch("aa", ".*") → true
+ isMatch("ab", ".*") → true
+ isMatch("aab", "c*a*b") → true
+ * The DP solution.
  */
 public class Solution3 {
+    public static boolean isMatch(String s, String p) {
+        boolean DP[][] = new boolean[s.length()+1][p.length()+1];
+        DP[0][0] = true;
+        for(int i=1;i<=s.length();i++){
+            DP[i][0] = false;
+        }
+        for(int j=1;j<=p.length();j++){
+            DP[0][j] = p.charAt(j-1)=='*' && DP[0][j-2] && j>1;
+        }
+        for(int i=1; i<s.length()+1; i++){
+            for(int j=1; j<p.length()+1; j++){
+                if(p.charAt(j-1) != '*'){
+                    DP[i][j] = (s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='.') && DP[i-1][j-1];
+                }else {
+//                    if(p.charAt(j-2)==s.charAt(i-1) || p.charAt(j-2)=='.'){
+//                        DP[i][j] = DP[i-1][j];
+//                    }else if (p.charAt(j-2)!=s.charAt(i-1) && p.charAt(j-2)!='.'){
+//                        DP[i][j] = DP[i][j-2];
+//                    }
+                    DP[i][j] = DP[i][j - 2] || (s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.') && DP[i - 1][j];
+
+                }
+            }
+        }
+        return DP[s.length()][p.length()];
+    }
+
+
+    public static void main(String [] args){
+        String s = "aaa";
+        String p = "ab*a*c*a";
+        System.out.println(isMatch(s,p));
+    }
 }
